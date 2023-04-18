35cc0cd256b6a7deca5bd3abec06cf94a1a0d269
Joshua-Pretorius <131198616+Joshua-Pretorius@users.noreply.github.com>
2023-04-18 23:22:16 +0200

    Create dashapp.py

---
+++ dashapp.py
@@ -0,0 +1,30 @@
+# import the necessary libraries
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# load the datasets
+airlines = pd.read_csv("https://raw.githubusercontent.com/jpatokal/openflights/master/data/airlines.dat",
+                       header=None, names=["Airline ID", "Name", "Alias", "IATA", "ICAO", "Callsign", "Country", "Active"])
+airports = pd.read_csv("https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat",
+                       header=None, names=["Airport ID", "Name", "City", "Country", "IATA", "ICAO", "Latitude", "Longitude", "Altitude", "Timezone", "DST", "Tz database time zone"])
+routes = pd.read_csv("https://raw.githubusercontent.com/jpatokal/openflights/master/data/routes.dat",
+                     header=None, names=["Airline", "Airline ID", "Source airport", "Source airport ID", "Destination airport", "Destination airport ID", "Codeshare", "Stops", "Equipment"])
+
+# create the web application using streamlit
+st.set_page_config(page_title="OpenFlights Dashboard", page_icon="✈️", layout="wide")
+st.title("OpenFlights Dashboard")
+
+# display the airline data in a chart
+st.header("Airline Data")
+st.write(airlines)
+st.subheader("Airline Count by Country")
+airline_counts = airlines.groupby("Country").size().reset_index(name="Count")
+fig1 = px.bar(airline_counts, x="Country", y="Count", color="Country", title="Airline Count by Country")
+st.plotly_chart(fig1)
+
+# display the airport data in a chart and map
+st.header("Airport Data")
+st.write(airports)
+st.subheader("Airport Count by Country")
+airport_counts = airports.groupby("Country").size().reset
